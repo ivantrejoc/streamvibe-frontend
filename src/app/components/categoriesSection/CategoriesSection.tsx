10ee9a5e2030b88155fcb83ac5e25575102d5840
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2025-03-04 12:47:10 +0100

    carousel tablet fixed

--- src/app/components/categoriesSection/CategoriesSection.tsx
+++ src/app/components/categoriesSection/CategoriesSection.tsx
@@ -34,7 +34,8 @@ export default function CategoriesSection() {
         breakpoints={{
           720: {
             slidesPerView: 2,
-            spaceBetween: 100
+            spaceBetween: 65,
+            centeredSlides: true
           },
           1024: {
             slidesPerView: 2,
@@ -49,6 +50,7 @@ export default function CategoriesSection() {
             spaceBetween: 100
           }
         }}
+        
       >
         <SwiperSlide className={styles.categoryCard}>
           <Link href="/">
@@ -58,6 +60,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/action/action-1.png"}
                   alt="action-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame2}>
@@ -65,6 +68,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/action/action-2.png"}
                   alt="action-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame3}>
@@ -72,6 +76,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/action/action-3.png"}
                   alt="action-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame4}>
@@ -79,6 +84,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/action/action-4.png"}
                   alt="action-movie"
+                  loading="lazy"
                 />
               </div>
             </div>
@@ -90,6 +96,7 @@ export default function CategoriesSection() {
                   height={25}
                   width={25}
                   alt="right-arrow"
+                  loading="lazy"
                 />
               </div>
             </div>
@@ -103,6 +110,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/adventure/adventure-1.png"}
                   alt="adventure-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame2}>
@@ -110,6 +118,7 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/adventure/adventure-2.png"}
                   alt="adventure-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame3}>
@@ -117,12 +126,14 @@ export default function CategoriesSection() {
                   fill
                   src={"/thumbnails/adventure/adventure-3.png"}
                   alt="adventure-movie"
+                  loading="lazy"
                 />
               </div>
               <div className={styles.frame4}>
                 <Image
+                  loading="lazy"
                   fill
-                  src={"/thumbnails/adventure/adventrure-4.png"}
+                  src={"/thumbnails/adventure/adventure-4.png"}
                   alt="adventure-movie"
                 />
               </div>
@@ -131,6 +142,7 @@ export default function CategoriesSection() {
               <h3 className={styles.categoryTitle}>Adventure</h3>
               <div className={styles.iconContainer}>
                 <Image
+                  loading="lazy"
                   src={"/arrow-small-right.png"}
                   height={25}
                   width={25}
@@ -145,6 +157,7 @@ export default function CategoriesSection() {
             <div className={styles.imagesContainer}>
               <div className={styles.frame1}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/comedy/comedy-1.png"}
                   alt="comedy-movie"
@@ -152,6 +165,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame2}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/comedy/comedy-2.png"}
                   alt="comedy-movie"
@@ -159,6 +173,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame3}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/comedy/comedy-3.png"}
                   alt="comedy-movie"
@@ -166,6 +181,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame4}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/comedy/comedy-4.png"}
                   alt="comedy-movie"
@@ -176,6 +192,7 @@ export default function CategoriesSection() {
               <h3 className={styles.categoryTitle}>Comedy</h3>
               <div className={styles.iconContainer}>
                 <Image
+                  loading="lazy"
                   src={"/arrow-small-right.png"}
                   height={25}
                   width={25}
@@ -190,6 +207,7 @@ export default function CategoriesSection() {
             <div className={styles.imagesContainer}>
               <div className={styles.frame1}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/drama/drama-1.png"}
                   alt="drama-movie"
@@ -197,6 +215,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame2}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/drama/drama-2.png"}
                   alt="drama-movie"
@@ -204,6 +223,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame3}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/drama/drama-3.png"}
                   alt="drama-movie"
@@ -211,6 +231,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame4}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/drama/drama-4.png"}
                   alt="drama-movie"
@@ -235,6 +256,7 @@ export default function CategoriesSection() {
             <div className={styles.imagesContainer}>
               <div className={styles.frame1}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/horror/horror-1.png"}
                   alt="action-movie"
@@ -242,6 +264,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame2}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/horror/horror-2.png"}
                   alt="action-movie"
@@ -249,6 +272,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame3}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/horror/horror-3.png"}
                   alt="action-movie"
@@ -256,6 +280,7 @@ export default function CategoriesSection() {
               </div>
               <div className={styles.frame4}>
                 <Image
+                  loading="lazy"
                   fill
                   src={"/thumbnails/horror/horror-4.png"}
                   alt="action-movie"
