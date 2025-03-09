1494fffcabec51074710f965ac953b23fb3affc8
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2025-03-09 16:56:34 +0100

    M&S trending slider mobile version

--- src/app/components/moviesAndShowsSlider/MoviesAndShowsSlider.tsx
+++ src/app/components/moviesAndShowsSlider/MoviesAndShowsSlider.tsx
@@ -40,79 +40,77 @@ export default function MoviesAndShowsSlider({ trendings }: TrendingProps) {
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className={styles.swiper}
       >
-        {trendings?.map((trending) => (
-          <SwiperSlide
-            key={trending.id}
-            className={styles.swiperSlide}
-            style={{
-              background: `linear-gradient(0deg, #141414 0%, rgba(20, 20, 20, 0.00) 100%), url("${imageBaseUrl}${trending.backdrop_path}") lightgray 50% / cover no-repeat`,
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              padding: "3.125rem 3.125rem 6.725rem 3.125rem",
-              flexDirection: "column",
-              justifyContent: "flex-end",
-              alignItems: "center",
-              gap: "3.125rem",
-              flexShrink: "0",
-              boxSizing: "border-box"
-            }}
-          >
-            <div className={styles.infoContainer}>
-              <div className={styles.textContainer}>
-                <h3 className={styles.title}>
-                  {trending.original_title || trending.original_name}
-                </h3>
-                <p className={styles.paragraph}>{trending.overview}</p>
-              </div>
-              <div className={styles.buttonsContainer}>
-                <Link href={"/"} passHref legacyBehavior>
-                  <a
-                    className={styles.playButton}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
+        {trendings?.map((trending) => {
+          const backgroundImage = `${imageBaseUrl}${trending.backdrop_path}`;
+          return (
+            <SwiperSlide
+              key={trending.id}
+              className={styles.swiperSlide}
+              style={{ backgroundImage: `url(${backgroundImage})` }}
+            >
+              <div className={styles.infoContainer}>
+                <div className={styles.textContainer}>
+                  <h3 className={styles.title}>
+                    {trending.original_title || trending.original_name}
+                  </h3>
+                  <p className={styles.paragraph}>{trending.overview}</p>
+                </div>
+                <div className={styles.buttonsContainer}>
+                  <Link href={"/"} passHref legacyBehavior>
+                    <a
+                      className={styles.playButton}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src={"/social-media-icons/play.png"}
+                        width={28}
+                        height={28}
+                        sizes="(max-width: 720px) 1.5rem, 1.5rem"
+                        alt="play-icon"
+                        loading="lazy"
+                      />
+                      Play Now
+                    </a>
+                  </Link>
+                  <div className={styles.actionButtonsContainer}>
+                  <button className={styles.actionButton}>
                     <Image
-                      src={"/social-media-icons/play.png"}
+                      src={"/plus.png"}
                       width={28}
                       height={28}
-                      alt="play-icon"
+                       sizes="(max-width: 720px) 1.5rem, 1.5rem"
+                      alt="plus-icon"
                       loading="lazy"
                     />
-                    Play Now
-                  </a>
-                </Link>
-                <button className={styles.actionButton}>
-                  <Image
-                    src={"/plus.png"}
-                    width={28}
-                    height={28}
-                    alt="plus-icon"
-                    loading="lazy"
-                  />
-                </button>
-                <button className={styles.actionButton}>
-                  <Image
-                    src={"/social-media-icons/hand-thumb-up.png"}
-                    width={28}
-                    height={28}
-                    alt="hand-up-icon"
-                    loading="lazy"
-                  />
-                </button>
-                <button className={styles.actionButton}>
-                  <Image
-                    src={"/social-media-icons/speaker-wave.png"}
-                    width={28}
-                    height={28}
-                    alt="speaker-wave-icon"
-                    loading="lazy"
-                  />
-                </button>
+                  </button>
+                  <button className={styles.actionButton}>
+                    <Image
+                      src={"/social-media-icons/hand-thumb-up.png"}
+                      width={28}
+                      height={28}
+                       sizes="(max-width: 720px) 1.5rem, 1.5rem"
+                      alt="hand-up-icon"
+                      loading="lazy"
+                    />
+                  </button>
+                  <button className={styles.actionButton}>
+                    <Image
+                      src={"/social-media-icons/speaker-wave.png"}
+                      width={28}
+                      height={28}
+                       sizes="(max-width: 720px) 1.5rem, 1.5rem"
+                      alt="speaker-wave-icon"
+                      loading="lazy"
+                    />
+                  </button>
+                  </div>
+                 
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
