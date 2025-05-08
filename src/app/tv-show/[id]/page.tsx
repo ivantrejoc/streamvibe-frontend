85e60e9bdcf13f15796bdee79d758ab9c2c9da67
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2025-05-08 10:50:58 +0200

    tv-show by id view

---
+++ src/app/tv-show/[id]/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../../../styles/views.module.css";
+export default function ShowDetails() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>HOLA SOY SHOW DETAILS</h1>
+      </main>
+    </div>
+  );
+}
